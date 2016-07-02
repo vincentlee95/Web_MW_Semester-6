8b859284ee3be14495f8ed6088e2780900041baa
Frandy <fran_xiong@yahoo.com>
2016-07-03 00:21:08 +0700

    login auth

--- instagram/config.js
+++ instagram/config.js
@@ -1,7 +1,7 @@
 var config = {
-  lob_api_key: 'test_985fb64e928ce97f4c23bb824c34faf3da8',
+  lob_api_key: 'test_d19cfa577825d2d9cd3a2360d074f411285',
   instagram_redirect_uri: 'http://localhost:3000/handleauth',
-  instagram_client_id: '9558d731716f4a1c99f9678626b8478d',
-  instagram_client_secret: '1e46b7da34964fa09bdb83c3e00fcc9d',
+  instagram_client_id: '1f176f7ede9a47f8a8ea2cd2bfeed8a',
+  instagram_client_secret: '546b31433c9b46759244e48b7fd0c429',
 }
 module.exports = config;
