3334e76572ff63618bbd6a5870a0952623942758
Edric Yawin <edric.win@gmail.com>
2016-07-03 01:08:24 +0700

    edit config.js

--- instagram/config.js
+++ instagram/config.js
@@ -1,12 +1,7 @@
 var config = {
   lob_api_key: 'test_d19cfa577825d2d9cd3a2360d074f411285',
   instagram_redirect_uri: 'http://localhost:3000/handleauth',
-<<<<<<< HEAD
   instagram_client_id: '12b40253b9174c82b61c525b81bad259',
   instagram_client_secret: 'b011b06992b44c17b9c25e414a537be8',
-=======
-  instagram_client_id: '1f176f7ede9a47f8a8ea2cd2bfeed8a',
-  instagram_client_secret: '546b31433c9b46759244e48b7fd0c429',
->>>>>>> ae2766275d92543a38816a8cc0466381b4fe0307
 }
 module.exports = config;
